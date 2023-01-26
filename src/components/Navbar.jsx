ddf6b605d821b73084891df275f1a7c290b2ebf3
kerime abanoz <krmeabnz@gmail.com>
2023-01-26 13:18:15 +0300

    pushlasana yaaa

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ import { useNavigate } from "react-router-dom";
 
 export default function Navbar() {
   const navigate = useNavigate();
-  const user = true;
+  const user = false;
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" color="secondary">
@@ -21,8 +21,8 @@ export default function Navbar() {
           >
             Clarusway News
           </Typography>
-
-          <Button color="inherit">Login</Button>
+          {user && <Button color="inherit">Logout</Button>}
+          {!user && <Button color="inherit">Login</Button>}
         </Toolbar>
       </AppBar>
     </Box>
