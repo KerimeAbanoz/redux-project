c0127f2b0e4a6364b1422c4a4947958903417c4f
kerime abanoz <krmeabnz@gmail.com>
2023-01-26 14:02:49 +0300

    neden olmuyor :(

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,6 +9,10 @@ import { useNavigate } from "react-router-dom";
 export default function Navbar() {
   const navigate = useNavigate();
   const user = false;
+
+  const handleLogout =()=> {
+    // todo
+  }
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" color="secondary">
@@ -21,8 +25,12 @@ export default function Navbar() {
           >
             Clarusway News
           </Typography>
-          {user && <Button color="inherit">Logout</Button>}
-          {!user && <Button color="inherit">Login</Button>}
+          {user && <Button color="inherit" onClick={handleLogout()}>Logout</Button>}
+          {!user && (
+            <Button color="inherit" onClick={() => navigate("/login")}>
+              Login
+            </Button>
+          )}
         </Toolbar>
       </AppBar>
     </Box>
